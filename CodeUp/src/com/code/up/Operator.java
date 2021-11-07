d3e8dc5c10a65d152667d6d4c3b3dcb70f81bed2
DESKTOP-QA0M42J\taewan <ntw2100@gmail.com>
2021-11-07 10:12:47 +0900

    codeup

--- CodeUp/src/com/code/up/Operator.java
+++ CodeUp/src/com/code/up/Operator.java
@@ -1,5 +1,6 @@
 package com.code.up;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Operator {
@@ -8,7 +9,15 @@ public class Operator {
 		return a+b;
 	}
 	
-	public static void main(String[] args) {
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 		
 		// 정수 2개 입력받아 합 출력하기1
@@ -21,13 +30,18 @@ public class Operator {
 //		System.out.println(sum(Integer.parseInt(split[0]), Integer.parseInt(split[1])));
 		
 		//정수 1개 입력받아 부호 바꿔 출력하기
-		int a = sc.nextInt();
-		if(a > 0) {
-			a = -a;
-		}else if(a<0) {
-			a = -(a);
-		}
-		System.out.printf("%d", a);
+//		int a = sc.nextInt();
+//		if(a > 0) {
+//			a = -a;
+//		}else if(a<0) {
+//			a = -(a);
+//		}
+//		System.out.printf("%d", a);
+		
+		int a = 3;
+		int b = 5;
+		
+		System.out.println(b%a);
 	}
 
 }
