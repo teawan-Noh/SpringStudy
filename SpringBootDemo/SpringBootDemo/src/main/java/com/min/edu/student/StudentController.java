aaba273a5d02659c04f30ef515141294ac74e007
DESKTOP-QA0M42J\taewan <ntw2100@gmail.com>
2021-12-27 16:02:37 +0900

    boot

--- SpringBootDemo/SpringBootDemo/src/main/java/com/min/edu/student/StudentController.java
+++ SpringBootDemo/SpringBootDemo/src/main/java/com/min/edu/student/StudentController.java
@@ -3,10 +3,14 @@ package com.min.edu.student;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,6 +32,19 @@ public class StudentController {
 	@PostMapping
 	public void addNewStudent(@RequestBody StudentDto studentDto) {
 		studentService.addNewStudent(studentDto);
+		
+	}
+	
+	@DeleteMapping(path = "{studentId}")
+	public void deltetStudent(@PathVariable("studentId") Long studentId) {
+		studentService.deleteStudent(studentId);
+	}
+	
+	@PutMapping(path = "{studentId}")
+	public void updateStudent(@PathVariable("studentId") Long studentId, 
+							@RequestParam(required = false) String name,
+							@RequestParam(required = false) String email) {
+		studentService.updateStudent(studentId, name, email);
 		
 	}
 }
--- SpringBootDemo/SpringBootDemo/src/main/java/com/min/edu/student/StudentService.java
+++ SpringBootDemo/SpringBootDemo/src/main/java/com/min/edu/student/StudentService.java
@@ -3,7 +3,10 @@ package com.min.edu.student;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -38,6 +41,32 @@ public class StudentService {
 			throw new IllegalMonitorStateException("사용중인 이메일");
 		}
 		studentRepository.save(student);
+		
+	}
+	
+	public void deleteStudent(Long studentId) {
+		boolean exists = studentRepository.existsById(studentId);
+		if(!exists) {
+			throw new IllegalArgumentException("학생이 존재하지 않습니다");
+		}
+		studentRepository.deleteById(studentId);
+	}
+	
+	@Transactional
+	public void updateStudent(Long studentId, String name, String email) {
+		StudentDto student = studentRepository.findById(studentId).orElseThrow(()->new IllegalStateException("해당"+studentId+"학생은 없습니다"));
+		
+		if(name != null && name.length()>0 && !Objects.equals(student.getName(), name)) {
+			student.setName(name);
+		}
+		if(email != null && email.length()>0 && !Objects.equals(student.getEmail(), email)) {
+			Optional<StudentDto> studentOptional = studentRepository.findStudentByEmail(email);
+			System.out.println(studentOptional.isPresent());
+			if(studentOptional.isPresent()) {
+				throw new IllegalStateException("존재하는 email 입니다");
+			}
+			student.setEmail(email);
+		}
 		
 	}
 }
